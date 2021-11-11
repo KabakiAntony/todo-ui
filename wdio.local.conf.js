244ae168ac96b3f1c76c4b43c1a8a8e24d32f7ab
KabakiAntony <kabaki.antony@gmail.com>
2021-11-11 18:24:29 +0300

    init

---
+++ wdio.local.conf.js
@@ -0,0 +1,19 @@
+const { config } = require('./wdio.shared.conf')
+
+exports.config = {
+  /**
+   * base config
+   */
+  ...config,
+  /**
+   * config for local testing
+   */
+  maxInstances: 1,
+  services: ['geckodriver'],
+  capabilities: [
+    {
+      browserName: 'firefox',
+      acceptInsecureCerts: true
+    }
+  ]
+}
