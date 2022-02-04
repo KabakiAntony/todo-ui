003f579a8ed612c21da6157819ce9d67b7ff0042
KabakiAntony <kabaki.antony@gmail.com>
2022-02-04 14:13:48 +0300

    removed img tag from the home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Todos UI Logo" src="../assets/images/undraw_welcome_cats_thqn.svg">
     <TodoIndex msg="Ain't this the best todo app on the internet"/>
-  </div>
 </template>
 
 <script>
@@ -17,9 +14,4 @@ export default {
 </script>
 
 <style scoped>
-  img {
-    margin-top: 5%;
-    height: 35%;
-    width: 70%;
-  }
 </style>>
