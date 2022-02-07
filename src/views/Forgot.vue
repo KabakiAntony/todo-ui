fa00126b22e37aa7383fef8471d038a34ef3acd8
KabakiAntony <kabaki.antony@gmail.com>
2022-02-07 11:59:16 +0300

    created a Forgot password view

---
+++ src/views/Forgot.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page">
+    <h1>forgot your password ?</h1>
+    <p>relax we got you, just enter your email below and we will send you a link shortly.</p>
+    <img alt="forgot password" src="../assets/images/undraw_forgot_password_re_hxwm.svg">
+    <ForgotForm class="forgot" />
+  </div>
+</template>
+
+<script>
+import ForgotForm from "@/components/ForgotForm.vue"
+
+
+export default {
+    name:'Forgot',
+    components : { ForgotForm }
+
+}
+</script>
+
+<style scoped>
+.page{
+    width: 90%;
+    margin: 0 auto;
+    margin-top:5%;
+}
+img{
+    width: 45%;
+    float:left;
+}
+p{
+    font-weight: bold;
+    font-size: 110%;
+}
+</style>
