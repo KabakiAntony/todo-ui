9b87efe6b227b3d17fa16ee221872c3267fbc429
KabakiAntony <kabaki.antony@gmail.com>
2022-02-07 11:58:27 +0300

    created a form for sending reset link email

---
+++ src/components/ForgotForm.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="form-container">
+        <h2 class="form-header">Forgot Password</h2>
+        <form @submit.prevent="handleSubmit">
+            <label>Email</label>
+            <input type="email" v-model="form.email" required>
+            <div>
+                <button type="submit" class="submit" :class="action">
+                {{ submit_text }}
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ForgotForm',
+    data(){
+        return{
+            form:{
+                email:'',
+            },
+            action:'',
+            submit_text:"Send email"
+        }
+    },
+    methods:{
+        async handleSubmit(){
+            this.$emit('submit', this.form)
+        }
+
+    }
+}
+</script>
+
+<style scoped>
+#form-container{
+    margin-top:10%;
+}
+</style>
