dc58af44998e31b41238448207229cc02e86498a
KabakiAntony <kabaki.antony@gmail.com>
2022-02-17 12:14:09 +0300

    added tests for the dashboard component

---
+++ tests/unit/dashboard.spec.js
@@ -0,0 +1,17 @@
+import { shallowMount, mount } from '@vue/test-utils'
+import Dashboard from '@/views/Dashboard.vue'
+
+
+
+describe("Dashboard.vue", ()=>{
+    it("test we are getting the app component by name",()=>{
+        const wrapper = shallowMount(Dashboard)
+        expect(wrapper.vm.$options.name).toMatch('Dashboard')
+    })
+    
+    it("test displaying a header on the dashboard", ()=>{
+        const wrapper = mount(Dashboard)
+        expect(wrapper.get('h1').text()).toContain("List things you want to")
+
+    })
+})
