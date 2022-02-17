5cccd72c9056394490ebbac7dca411a27e803113
KabakiAntony <kabaki.antony@gmail.com>
2022-02-17 12:13:26 +0300

    created the dashboard component

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="dashboard">
+    <h1>List things you want to achieve today or cross them off your list</h1>
+    <AddTodo />
+    <TodoList />
+</div>
+</template>
+
+<script>
+import AddTodo from '@/components/AddTodo.vue'
+import TodoList from '@/components/TodoList.vue'
+
+
+export default {
+    name: "Dashboard",
+    components:{ AddTodo, TodoList },
+
+}
+</script>
+
+<style>
+
+</style>
