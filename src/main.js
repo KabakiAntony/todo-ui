9b50633739d86b69b95d674c2174c082e7be0b8e
KabakiAntony <kabaki.antony@gmail.com>
2022-02-21 11:18:05 +0300

    activated vuex store for use and configured api properties

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import './assets/css/style.css' /* this will be used incase I use a site style file*/
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(store).use(router).mount("#app");
+
+// app.config.globalProperties.$api = "http://127.0.0.1:5000/"
+// store.$api = "http://127.0.0.1:5000/"
+app.config.globalProperties.$api = "https://ka-todos.herokuapp.com/"
+store.$api = "https://ka-todos.herokuapp.com/"
