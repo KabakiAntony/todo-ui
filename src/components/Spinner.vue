11844b2e281ad2da1b746ab25667fcebba5841c7
KabakiAntony <kabaki.antony@gmail.com>
2022-02-22 13:48:30 +0300

    created the spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="spinner" id="show-spinner">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+      </div>
+</template>
+
+<script>
+export default {
+    name: "Spinner",
+
+}
+</script>
+
+<style>
+
+</style>
