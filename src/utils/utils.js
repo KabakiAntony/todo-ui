f551c84b05c8b6434871ce9d48d87222433c5495
KabakiAntony <kabaki.antony@gmail.com>
2022-02-22 13:49:17 +0300

    created the utilities folder and utility functions for loading and unloading spinner

---
+++ src/utils/utils.js
@@ -0,0 +1,7 @@
+export function loadSpinner(){
+    document.getElementById('show-spinner').style.display = " block";
+  }
+
+  export function unloadSpinner(){
+    document.getElementById('show-spinner').style.display = " none";
+  }
