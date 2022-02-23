e90ed0d5d365d1a969263d89426709f524c16c93
KabakiAntony <kabaki.antony@gmail.com>
2022-02-23 20:52:25 +0300

    deleted utils.js to use index.js to allow imports

--- src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export function loadSpinner(){
-    document.getElementById('show-spinner').style.display = " block";
-  }
-
-  export function unloadSpinner(){
-    document.getElementById('show-spinner').style.display = " none";
-  }
