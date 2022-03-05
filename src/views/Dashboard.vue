7b0ff570f233342ab13a6238b3032e468e548794
KabakiAntony <kabaki.antony@gmail.com>
2022-03-05 18:18:39 +0300

    implemented all functionalities for todos , that is created, read, update, delete

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,8 +5,14 @@
       <ShowAlert  v-if='show' :class='type' :message='message'/>
     </transition>
     <h1>List things you want to achieve today or cross them off your list</h1>
-    <AddTodo />
-    <TodoList />
+    <AddTodo @on-submit="addTodo"/>
+    <div v-if="todos.length">
+        <TodoList :todos='todos'  @on-update="updateTodo" @on-delete="deleteTodo"/>
+    </div>
+    <div v-else>
+        <p>We found no todos for you, Please add some.</p>
+    </div>
+    
 </div>
 </template>
 
@@ -25,13 +31,81 @@ export default {
             type:null,
             message:null,
             show:false,
-            action:null,
+            todos:[],
+            button_color: '',
+            button_text:'',
+
         }
-    }
+    },
+    methods:{
+        async addTodo(theForm){
+            const url = `${this.$api}todos`
+            const res = await fetch(url,{
+                method:'POST',
+                headers:{
+                    'Content-Type': 'application/json',
+                    'auth_token':this.$store.getters.AuthToken,
+                    },
+                    body: JSON.stringify(theForm)
+                    })
+            const data = await res.json()
+            if(data.status === 201){
+                await this.$store.dispatch('getTodos')
+                this.todos = this.$store.getters.Todos
+                theForm.text = ""
+            } else {
+              console.log(data.error)
+              }
+        },
+        async deleteTodo(todoId){
+            if(confirm('Are you sure you want to delete ?')){
+                const url = `${this.$api}todos/${todoId}`
+                const res = await fetch(url,{
+                    method:'DELETE',
+                    headers:{
+                        'Content-Type': 'application/json',
+                        'auth_token':this.$store.getters.AuthToken,
+                        },
+                        })
+                const data = await res.json()
+                if(data.status === 200){
+                    await this.$store.dispatch('getTodos')
+                    this.todos = this.$store.getters.Todos
+                } else {
+                    console.log(data.error)
+                }
+            }
+            
+        },
+        async updateTodo(todoId, todo){
+            const updated_todo = {
+                completed: !todo.completed,
+                id: todo.id,
+                text: todo.text,
+                user_id: todo.user_id
+            }
+            const url = `${this.$api}todos/${todoId}`
+            const res = await fetch(url,{
+                method:'PUT',
+                headers:{
+                    'Content-Type': 'application/json',
+                    'auth_token':this.$store.getters.AuthToken,
+                    },
+                    body: JSON.stringify(updated_todo)
+                    })
+            const data = await res.json()
+            if(data.status === 200){
+                await this.$store.dispatch('getTodos')
+                this.todos = this.$store.getters.Todos
+            } else {
+                console.log(data.error)
+            }
+        }
+    },
+    async created(){
+      await this.$store.dispatch('getTodos')
+      this.todos = this.$store.getters.Todos
+    },
 
 }
 </script>
-
-<style>
-
-</style>
