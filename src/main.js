695bb99d1e321f2c075bd9ff2825b22c99c30f57
KabakiAntony <kabaki.antony@gmail.com>
2022-03-07 18:45:59 +0300

    changed the api route to the production link

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import './assets/css/style.css' /* this will be used incase I use a site style file*/
 
 const app = createApp(App)
 app.use(store).use(router).mount("#app");
 
-app.config.globalProperties.$api = "http://127.0.0.1:5000/"
-store.$api = "http://127.0.0.1:5000/"
-// app.config.globalProperties.$api = "https://ka-todos.herokuapp.com/"
-// store.$api = "https://ka-todos.herokuapp.com/"
+// app.config.globalProperties.$api = "http://127.0.0.1:5000/"
+// store.$api = "http://127.0.0.1:5000/"
+app.config.globalProperties.$api = "https://ka-todos.herokuapp.com/"
+store.$api = "https://ka-todos.herokuapp.com/"
