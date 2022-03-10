a1e723c7540d3808f68aae4e4d18e33d71ed9c5e
KabakiAntony <kabaki.antony@gmail.com>
2022-03-10 18:38:01 +0300

    renamed todoindex to index vue

---
+++ src/components/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page">
+    <img alt="Todos UI Logo" src="../assets/images/undraw_welcome_cats_thqn.svg">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  props: {
+    msg: String
+  }
+}
+</script>
