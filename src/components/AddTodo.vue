9874c609883d8949b130b8d505e1acb082edb734
KabakiAntony <kabaki.antony@gmail.com>
2022-03-21 22:26:01 +0300

    added data-test notation to show what parts are used in tests

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="add-todo">
       <form @submit.prevent="$emit('on-submit', form)">
-          <input type="text" v-model="form.text" required>
+          <input type="text" v-model="form.text"  data-test="new-todo" required>
           <button type="submit"  class="add-todo-submit" :class="action">{{ submit_text }}</button>
       </form>
   </div>
