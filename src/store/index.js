a2c0305e87c3ed88b986755e3316cee28babddcd
KabakiAntony <kabaki.antony@gmail.com>
2022-03-22 12:25:10 +0300

    removed redeclaration of store object

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,6 @@ import auth from './modules/auth';
 import todos from './modules/todos';
 import VuexPersistence from 'vuex-persist'
 
-
-// we can create a vuexpersisted state object
-// or use it directly as  below in store
-// object
-// const vuexLocal = new VuexPersistence({
-//   storage: window.localStorage,
-// });
-
 const store = createStore({
   modules: {
     auth,
