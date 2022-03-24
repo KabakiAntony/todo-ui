8bf7b59eee5d5e590fa0794b2dd7fd26b2d7764d
KabakiAntony <kabaki.antony@gmail.com>
2022-03-24 18:02:31 +0300

    testing that components used on the dashboard component are being rendered

--- tests/unit/dashboard.spec.js
+++ tests/unit/dashboard.spec.js
@@ -1,5 +1,7 @@
 import { shallowMount, mount } from '@vue/test-utils'
 import Dashboard from '@/views/Dashboard.vue'
+import AddTodo from '../../src/components/AddTodo.vue'
+import TodoList from '../../src/components/TodoList.vue'
 
 
 
@@ -14,4 +16,32 @@ describe("Dashboard.vue", ()=>{
         expect(wrapper.get('h1').text()).toContain("List things you want to")
 
     })
+     
+    it("test that AddTodo is rendered",()=>{
+        const wrapper = mount(Dashboard)
+        expect(wrapper.findComponent(AddTodo).exists()).toBe(true)
+    })
+
+    it("test that TodoList is not rendered when there are no todos",()=>{
+        const wrapper = mount(Dashboard)
+        expect(wrapper.findComponent(TodoList).exists()).toBe(false)
+    })
+    it("test that todo is rendered  when passed as a prop and as a component", ()=>{
+        const wrapper = mount(TodoList,{
+            props:{
+                todos: [
+                    {
+                        id: 1,
+                        text: 'Learn Vue.js 3',
+                        completed: false
+                      }
+                ]
+            }
+        })
+        const dashboard_wrapper = mount(Dashboard)
+        expect(dashboard_wrapper.findComponent(TodoList).exists()).toBe(false)
+        const todo = wrapper.get('[data-test="todo"]')
+        expect(todo.text()).toBe('Learn Vue.js 3')
+    })
+
 })
