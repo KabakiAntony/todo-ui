aae30e30bb289320b3fcd164437b213f4eeefa39
KabakiAntony <kabaki.antony@gmail.com>
2022-04-06 13:59:48 +0300

    recreated the auth store

---
+++ src/store/modules/auth/getters.js
@@ -0,0 +1,11 @@
+export const AuthToken = (state) =>{
+    return state.auth_token
+}
+
+export const IsLoggedIn = (state) =>{
+    return state.isLoggedIn
+}
+
+export const ScreenName = (state) =>{
+    return state.screen_name
+}
---
+++ src/store/modules/auth/state.js
@@ -0,0 +1,5 @@
+export default {
+    auth_token:null,
+    isLoggedIn :false,
+    screen_name:null,
+}
