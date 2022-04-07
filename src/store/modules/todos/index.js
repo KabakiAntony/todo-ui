9a43f2a8322ee9479263f03fdcef13b5c4579ab2
KabakiAntony <kabaki.antony@gmail.com>
2022-04-07 15:05:40 +0300

    condensed all todos store module actions into one file

--- src/store/modules/todos/index.js
+++ src/store/modules/todos/index.js
@@ -1,11 +1,57 @@
-import state from './state'
-import * as getters from './getters'
-import * as  actions from './actions'
-import * as  mutations from './mutations'
+const state = {        
+    todos:[]
+}
 
-export default {
-    state,
-    actions,
-    getters,
-    mutations
+const getters = {
+    Todos(state){
+        return state.todos
+    }
+}
+
+const actions = {
+   async getTodos ({commit, rootState}){
+        const token = rootState.auth.auth_token
+        try{
+          const url = `${this.$api}todos`
+          const res = await fetch(url,{
+          method:'GET',
+          headers:{
+            'auth_token': token
+          },
+          })
+          const data = await res.json()
+          if (data.status === 200){
+              commit('SET_TODOS',data.data)
+          }
+          else if(data.status === 404){
+            commit('RESET_TODOS')
+          }
+          return data
+        }
+        catch(err)
+        {
+          return err
+    
+        }
+     }
+}
+
+const mutations = {
+    SET_TODOS (state,payload){
+        state.todos = payload
+     },
+
+    RESET_TODOS(state){
+        state.todos.length = 0;
+     }
+}
+
+
+
+export default{
+  // namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
 }
